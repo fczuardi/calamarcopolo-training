f7305cd5e23be4dc5dfdd1f897d711d6551021ec
Fabricio C Zuardi <fabricio@fabricio.org>
2016-05-04 16:55:03 -0300

    shell script to add multiple values to a wit entity based on a json file with an array

---
+++ tasks/add-entity-values.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -euv
+
+entityName=$1
+valuesFile=$2
+
+function addEntityValue {
+    entityValue=$1
+    curl -XPOST "https://api.wit.ai/entities/$entityName/values" \
+    -H "Authorization: Bearer $WIT_TOKEN" \
+    -H 'Content-Type: application/json' \
+    -d '{ "value":"'"$entityValue"'" }' \
+    --connect-timeout 10
+}
+
+# change IFS so for loops don't separate use spaces as separators
+IFS=$'\n'
+for value in $(jq -r '.[]' "$valuesFile"); do
+    echo "addEntityValue $value"
+    addEntityValue "$value"
+    # break
+done
