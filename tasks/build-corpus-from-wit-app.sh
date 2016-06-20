8715210d1eb2949ed08d84572596de942655bbc4
Fabricio C Zuardi <fabricio@fabricio.org>
2016-06-20 09:35:51 -0300

    corpus update and botname placeholder replacement for the word "bigode"

--- tasks/build-corpus-from-wit-app.sh
+++ tasks/build-corpus-from-wit-app.sh
@@ -2,6 +2,6 @@
 set -euv
 
 curl -s -N "https://api.wit.ai/corpus" -H "Authorization: Bearer $WIT_TOKEN"\
-| jq '[.[] | sub("^@\\w* ?";"")]|unique' \
+| jq '[.[] | sub("^@\\w* ?";"") | sub("bigode";"BOTNAME";"i")]|unique' \
 > ./data/corpus-wit.json
 echo $(jq '.|length' data/corpus-wit.json) statements
